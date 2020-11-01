55fa5082b799a371a17a313c609227257ea9d5c9
alex velikanov <alxs13@yandex.ru>
2020-11-01 18:29:47 +0300

    frst

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let popup = document.querySelector('.popup');
+let popupContent = document.querySelector('.popup__content');
+let popupTitle = document.querySelector('.popup__title');
+
+let form = document.querySelector('.popup__form');
+let nameField = document.querySelector('.popup__input_type_name');
+let profField = document.querySelector('.popup__input_type_prof');
+
+let popupCloseButton = document.querySelector('.popup__close');
+
+let EditButton = document.querySelector('.profile__button');
+
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+
+function showPopup() {
+    popup.classList.add('popup_opened');
+    popup.removeEventListener('click', showPopup);
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+EditButton.addEventListener('click', showPopup);
+popupCloseButton.addEventListener('click', closePopup);
+
+
+function submitForm(event) {
+    event.preventDefault();
+    title.textContent = nameField.value;
+    subtitle.textContent = profField.value;
+    closePopup();
+}
+
+form.addEventListener('submit', submitForm);
