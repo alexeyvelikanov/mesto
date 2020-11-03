a1bc404bd717daf9e397592257ddb4b9231a1d64
alex velikanov <alxs13@yandex.ru>
2020-11-03 22:07:59 +0300

    exch

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,24 @@
 let popup = document.querySelector('.popup');
 let popupContent = document.querySelector('.popup__content');
 let popupTitle = document.querySelector('.popup__title');
-
 let form = document.querySelector('.popup__form');
 let nameField = document.querySelector('.popup__input_type_name');
 let profField = document.querySelector('.popup__input_type_prof');
-
 let popupCloseButton = document.querySelector('.popup__close');
-
 let EditButton = document.querySelector('.profile__button');
-
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
 
 function showPopup() {
+    nameField.value = title.textContent;
+    profField.value = subtitle.textContent;
     popup.classList.add('popup_opened');
-    popup.removeEventListener('click', showPopup);
+    //popup.removeEventListener('click', showPopup);
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
-
-EditButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
 
 
 function submitForm(event) {
@@ -33,4 +28,7 @@ function submitForm(event) {
     closePopup();
 }
 
+EditButton.addEventListener('click', showPopup);
+popupCloseButton.addEventListener('click', closePopup);
 form.addEventListener('submit', submitForm);
+
